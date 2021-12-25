86e28b6474598b5e150cdf18d5efa808ebe6bcb9
Tris Emmy Wilson <tris@tris.fyi>
2021-12-24 21:41:44 -0600

    add nix-reconfigure helper

--- shell.nix
+++ shell.nix
@@ -1,8 +1,14 @@
 { pkgs ? import <nixpkgs> {} }:
 
-pkgs.mkShell {
+let
+  nix-reconfigure = pkgs.writeShellScriptBin "nix-reconfigure" ''
+    scp $2 root@$1:/etc/nixos/configuration.nix
+    ssh root@$1 nixos-rebuild switch
+  '';
+in pkgs.mkShell {
   buildInputs = with pkgs; [
     awscli2
+    nix-reconfigure
     terraform
   ];
 }
